657a3cd38afff413cabadd699b5566e50b12176e
dorian.di-russo <71875004+dorian-di-russo@users.noreply.github.com>
2020-11-02 15:46:48 +0100

    Add files via upload
    du php

---
+++ jour01/job01/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+$str2= 'Vive ';
+echo $str2;
+
+$str = 'Laplateforme ';
+echo $str;
+
+$str3= '! <br> ';
+echo $str3;
+
+
+$val=  6+4;
+echo $val;
+
+$myBool= false;
+echo $myBool;
+
+
+
+
+
+
+
+?>
+
+
+
---
+++ jour01/job02/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$platform= 'Hello LaPlateforme!<br>';
+#  du php  
+echo  str_repeat("$platform", 2);
+
+
+// du php
+
+?>
