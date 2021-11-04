235e36629ec42c150848cd565778189f07c2bd23
dorian.di-russo <71875004+dorian-di-russo@users.noreply.github.com>
2021-11-04 23:19:52 +0100

    Add files via upload

---
+++ jour03/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$i = array(200, 204, 173, 98, 171, 404, 459);
+
+foreach($i as $value)
+    {
+             if ($value % 2 == 0) 
+
+             {
+                 echo $value . "est pair" . '<br>';  
+             }
+
+             elseif ($value % 2 !=  0 ) 
+
+             {
+                 echo $value . 'est impair'. '<br>';
+             }
+
+    }
+    var_dump($)
+
+?>
